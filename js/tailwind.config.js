99154a77e27862b57f408f2e0f67472aeb2366a8
MD Ashik <info.freelancerashik@gmail.com>
2024-07-03 14:53:46 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        btn:'#6A4DF4',
+        bannerOverley:'rgba(0, 0, 0, 0.597)',
+        contentOne:'#E0FFF9',
+        offWhite:'#737373',
+        bgoffWhite:'#F3F3F3',
+        star:'#F9BD2C',
+        review:'#F4EFEF',
+        contac:'#DCDCDC',
+        inputBorder:'#E8E8E8',
+      },
+      maxWidth: {
+        container: '1320px',
+      },
+      fontFamily: {
+        inter: ["inter"],
+        },
+        backgroundImage: {
+          bannerImage: "url('./images/banner.png')",
+          },
+          strokeWidth: {
+            icon: '<i class="fa-solid fa-quote-left"></i>',
+          }
+        }
+      }
+    }
